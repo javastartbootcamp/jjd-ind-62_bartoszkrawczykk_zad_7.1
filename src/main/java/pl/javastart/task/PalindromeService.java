629cc7e25327b03869d3688e4a94497caa0e7f48
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-03-14 20:48:05 +0100

    rozwiązanie

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -3,7 +3,26 @@ package pl.javastart.task;
 public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
     public boolean isPalindrome(int[] array) {
-        return false;
+        boolean palindrom = false;
+        if (array.length % 2 == 0) {
+            for (int i = 0; i < array.length / 2 - 1; i++) {
+                if (array[i] != array[array.length - i - 1]) {
+                    return false;
+                } else {
+                    palindrom = true;
+                }
+            }
+        } else {
+            for (int i = 0; i < (array.length - 1) / 2 - 1; i++) {
+                if (array[i] != array[array.length - i - 1]) {
+                    return false;
+                } else {
+                    palindrom = true;
+                }
+            }
+        }
+        return palindrom;
     }
 }
