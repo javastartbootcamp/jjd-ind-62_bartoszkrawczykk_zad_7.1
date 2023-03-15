4ff06f17e8a744a6ba8084813e5270debe6459e8
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-03-15 12:48:06 +0100

    rozwiązanie 2 pozostała mi sprawa z nullem którą nie do końca wiem jak rozwiązać

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -3,11 +3,10 @@ package pl.javastart.task;
 public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
-    @SuppressWarnings("checkstyle:WhitespaceAfter")
     public boolean isPalindrome(int[] array) {
         boolean palindrom = false;
-        if (array.length % 2 == 0) {
-            for (int i = 0; i < array.length / 2 - 1; i++) {
+        if ((array == null) && ((array.length % 2) == 0)) {
+            for (int i = 0; i < array.length / 2; i++) {
                 if (array[i] != array[array.length - i - 1]) {
                     return false;
                 } else {
@@ -15,7 +14,7 @@ public class PalindromeService {
                 }
             }
         } else {
-            for (int i = 0; i < (array.length - 1) / 2 - 1; i++) {
+            for (int i = 0; i < (array.length - 1) / 2; i++) {
                 if (array[i] != array[array.length - i - 1]) {
                     return false;
                 } else {
